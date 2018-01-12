3a85cd3981eeff98887986f1348d1286064ca97f
thiagobarso <thiagobarso@gmail.com>
2018-01-12 11:02:55 -0200

    adicinando modulo path do nodejs

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
+
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req, res){
-    res.sendFile(__dirname + '/dist/index.html');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 4200);
